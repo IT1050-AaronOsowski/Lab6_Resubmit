e4caa276befed5ba82ec6d271b396419c4b003c5
IT1050-AaronOsowski <aaron.osowski@yahoo.com>
2017-11-17 11:02:08 -0500

    Add project files.

---
+++ Lab6_Resubmit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab6_Resubmit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
